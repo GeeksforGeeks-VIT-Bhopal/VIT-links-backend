ca98be789f521056aa466fd3589584889f47f58a
Sajal Gupta <guptasajal411@gmail.com>
2021-10-03 10:35:28 +0530

    feat: :rocket: Read & send data as array from public/data

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({"name": "Sajal"});
+    var response = [];
+    const allData = fs.readdirSync("./public/data").filter(file => path.extname(file) === ".json");
+    console.log(allData);
+    allData.map((person) => {
+        const fileData = fs.readFileSync(path.join('./public/data', person));
+        const personData = JSON.parse(fileData.toString());
+        response.push(personData);
+    });
+    res.json(response);
 });
 
 app.listen(3001, () => {
